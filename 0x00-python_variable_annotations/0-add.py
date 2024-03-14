c15668e54346eb16419389d10b04e545db16b6d4
Nimi Williams <williamson.nimi@gmail.com>
2024-03-14 20:36:09 +0100

    Fixing requirement issue on 0-add.py

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
+""" This is the first add annotation module"""
+
+
 def add(a: float, b: float) -> float:
+    """ This is an annotated add function
+    @a: a float to be added
+    @b: a float to be added
+    Return: sum of both arguments should be a float
+    """
     return (a + b)
-
-
-if __name__ == "__main__":
-    add(a, b)
