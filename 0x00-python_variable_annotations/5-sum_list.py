b68ccbe371b9549d7f2f774a2cddc3575bfe0db1
Nimi Williams <williamson.nimi@gmail.com>
2024-03-14 21:10:31 +0100

    Fixed import issues in 5 and 6

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """ This is a complex types Module"""
+
+
 from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """ a sum of list function
     @input_list: The list of floats to be summed
     Return: Returns the sum of lists
--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -3,7 +3,7 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: list[Union[int, float]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """ this is a mixed list function
     @mxd_lst: A mixed list of numbers to be summed
     Return: Returns the sum of the numbers
