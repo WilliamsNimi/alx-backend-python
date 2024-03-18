2f9b5dd57585039107101a108b8f2a3fdd97c302
Nimi Williams <williamson.nimi@gmail.com>
2024-03-18 22:32:33 +0100

    Async await python exercises

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Concurrent coroutines """
+import asyncio
+
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int):
+    """ This function explores concurrency
+    @n: Number of times to call the async wait_random function
+    @max_delay: integer meant as parameter for the async calls
+    Return: Returns a list of delays from task_wait_random
+    """
+    delays = []
+    for x in range(0, n):
+        delays.append(await task_wait_random(max_delay))
+
+    return (delays)
